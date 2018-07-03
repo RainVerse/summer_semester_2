dd48690edb8a59125574abd02908f95f27f28b63
Rain <364687927@qq.com>
2018-07-03 10:41:05 +0800

    add database

--- dsa_test.py
+++ dsa_test.py
@@ -1,19 +1,32 @@
 from Crypto.Random import random
 from Crypto.PublicKey import DSA
-from Crypto.Hash import SHA
+from Crypto.Hash import SHA256
 
 
-message = 'Hello'
+message = 'Hello232asdasdadadsasdasdadasdad34234242342423'
 m2='hell'
-h = SHA.new(message.encode('utf-8')).digest()
-h2= SHA.new(m2.encode('utf-8')).digest()
+h = SHA256.new(message.encode('utf-8')).digest()
+print(len(h))
+h2= SHA256.new(m2.encode('utf-8')).digest()
 key = DSA.generate(1024)
 k = random.StrongRandom().randint(1, key.q-1)
 sig = key.sign(h, k)
-print(h)
-print(k)
-print(sig[0],sig[1])
-if key.verify(h2, sig):
+print(len(str(sig[0])),len(str(sig[1])))
+
+print(len(str(key.__getattr__('y'))))
+print(len(str(key.__getattr__('y'))))
+print(len(str(key.__getattr__('y'))))
+print(len(str(key.__getattr__('y'))))
+print(len(str(key.__getattr__('y'))))
+
+
+y=int(str(key.__getattr__('y')))
+g=int(str(key.__getattr__('g')))
+p=int(str(key.__getattr__('p')))
+q=int(str(key.__getattr__('q')))
+x=int(str(key.__getattr__('x')))
+
+if DSA.construct((y,g,p,q,x)).verify(h, sig):
     print('OK')
 else:
     print('Invalid Signature')
