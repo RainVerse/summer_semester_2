9bad7a91e2f35994f2bceba1831c7a49d1bcc2f4
Preknight <1073530029@qq.com>
2018-07-11 12:04:50 +0800

    登录、注册

--- digital_record_system.py
+++ digital_record_system.py
@@ -1,10 +1,19 @@
 from widgets.main_widget import MainWidget
+from widgets.Register import Register
+from widgets.InfoWindow import InfoWindow
+from widgets.Login import Login
 
 
 class DigitalRecordSystem:
     def __init__(self):
         self.main_window = MainWidget()
+        self.reg = Register()
+        self.iw = InfoWindow()
+        self.lgin = Login()
         return
 
     def init_slots(self):
+        self.reg.signal_user.connect(self.iw.show)
+        self.reg.signal_user.connect(self.iw.getUser())
+        self.lgin.regbtn.connect(self.reg.show)
         return
--- widgets/InfoWindow.py
+++ widgets/InfoWindow.py
@@ -1,36 +1,24 @@
-import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QCoreApplication
 from service.RegisterService import RegisterService
 from sql.sql_functions import get_department_list
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-class Register(QWidget):
 
+class InfoWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-
-        user = QLabel('用户名')
-        password = QLabel('密码')
-        ackpw = QLabel('确认密码')
         name = QLabel('姓名')
         gender = QLabel('性别')
         age = QLabel('年龄')
         department = QLabel('科室')
 
-        self.userEdit = QLineEdit(self)
-        self.pwEdit = QLineEdit(self)
-        self.ackpwEdit = QLineEdit(self)
         self.nameEdit = QLineEdit(self)
         self.genderEdit = QComboBox(self)
         self.ageEdit = QLineEdit(self)
         self.departmentEdit = QComboBox(self)
-
-        # 设置为密码模式
-        self.pwEdit.setEchoMode(2)
-        self.ackpwEdit.setEchoMode(2)
 
         self.genderEdit.setEditable(0)
         self.departmentEdit.setEditable(0)
@@ -43,11 +31,7 @@ class Register(QWidget):
             print(d)
             self.departmentEdit.addItem(d[0])
 
-
         form = QFormLayout()
-        form.addRow(user,self.userEdit)
-        form.addRow(password,self.pwEdit)
-        form.addRow(ackpw,self.ackpwEdit)
         form.addRow(name,self.nameEdit)
         form.addRow(gender,self.genderEdit)
         form.addRow(age,self.ageEdit)
@@ -56,23 +40,16 @@ class Register(QWidget):
         vbox = QVBoxLayout()
         vbox.addLayout(form)
         vbox.addSpacing(10)
-        #self.setLayout(form)
 
-
-
-
-
-        btn = QPushButton('注册',self)
-        btn.resize(btn.sizeHint())
-        btn.clicked.connect(self.transInfo)
+        self.tkonbtn = QPushButton('提交', self)
+        self.tkonbtn.resize(self.tkonbtn.sizeHint())
 
         cancelbtn = QPushButton('退出')
         cancelbtn.resize((cancelbtn.sizeHint()))
-        cancelbtn.clicked.connect(QCoreApplication.instance().quit)
 
         hbox = QHBoxLayout()
         hbox.addStretch(1)
-        hbox.addWidget(btn)
+        hbox.addWidget(self.tkonbtn)
         hbox.addWidget(cancelbtn)
         hbox.addStretch(1)
 
@@ -81,28 +58,29 @@ class Register(QWidget):
 
         self.setLayout(vbox)
 
-        self.resize(350,400)
+        self.resize(350, 400)
         self.center()
-        self.setWindowTitle('注册')
-        self.show()
+        self.setWindowTitle('提交信息')
 
+        self.tkonbtn.clicked.connect(self.transInfo())
+        cancelbtn.clicked.connect(QCoreApplication.instance().quit)
+
+    #def infoWindow(self):
+        #self.show()
+    def getUser(self, username, password):
+        print("get user")
+        self.username = username
+        self.password = password
+
+    def transInfo(self):
+        print("trans info to database")
+        source = self.sender()
+        regService = RegisterService()
+
+        userInfo = [self.nameEdit.text(), self.departmentlist[self.departmentEdit.currentIndex()][1], self.genderEdit.currentIndex() + 1, self.ageEdit.text()]
+        regService.register(self.username, self.password, userInfo)
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-
-    def transInfo(self):
-        source = self.sender()
-        regService = RegisterService()
-
-        userInfo = [self.nameEdit.text(), self.departmentlist[self.departmentEdit.currentIndex()][1], self.genderEdit.currentIndex() + 1, self.ageEdit.text()]
-        regService.register(self.userEdit.text(), self.pwEdit.text(), userInfo)
-
-
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    reg = Register()
-    sys.exit(app.exec_())
