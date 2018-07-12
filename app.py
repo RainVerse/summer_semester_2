ea6d1f4130c6f93a07c81af753d86fa4edcbd6d7
Preknight <1073530029@qq.com>
2018-07-12 10:43:09 +0800

    登录、注册

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from digital_record_system import DigitalRecordSystem
 
 app = QApplication(sys.argv)
 system = DigitalRecordSystem()
-system.main_window.set_user_id(233)
-system.main_window.show()
+system.login_widget.show()
+#system.main_widget.set_user_id(233)
+#system.main_widget.show()
+#system.register_widget.show()
 sys.exit(app.exec_())
